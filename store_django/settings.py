7788fe2e1d27ba711d10e8bfb95223fc15c34243
seb <sebduran@protonmail.com>
2021-07-07 20:26:58 -0400

    Procfile

--- store_django/settings.py
+++ store_django/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for store_django project.
 
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
